fb705f5fde5c759640dea3a1f7a4aca4999d0018
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-12 21:47:15 -0400

    Initial Commit

---
+++ WordScramblePractice/WordScramblePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblePracticeApp.swift
+//  WordScramblePractice
+//
+//  Created by Jordan Haynes on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
